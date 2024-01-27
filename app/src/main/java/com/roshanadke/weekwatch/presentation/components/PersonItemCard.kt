0698936f181e05286a870776c027c367e04297e4
roshan129 <roshan2015@gmail.com>
2024-01-27 19:45:31 +0530

    popular people screen done.

---
+++ app/src/main/java/com/roshanadke/weekwatch/presentation/components/PersonItemCard.kt
@@ -0,0 +1,82 @@
+package com.roshanadke.weekwatch.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.roshanadke.weekwatch.R
+import com.roshanadke.weekwatch.data.network.TrendingShowApiService
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PersonItemCard(
+    modifier: Modifier,
+    imageEndpoint: String?,
+    personName: String?,
+    onCardClicked: () -> Unit,
+) {
+
+    val imageUrl = TrendingShowApiService.POSTER_IMAGE_BASE_URL + imageEndpoint
+
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        onClick = onCardClicked
+    ) {
+
+        Box {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+            ) {
+
+                AsyncImage(
+                    placeholder = painterResource(id = R.drawable.loading_placeholder),
+                    model = imageUrl,
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                )
+
+                Row(
+                    Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = personName ?: "",
+                        modifier = Modifier
+                            .padding(horizontal = 6.dp, vertical = 8.dp)
+                            .fillMaxWidth(),
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/roshanadke/weekwatch/presentation/screens/PeopleListScreen.kt
+++ app/src/main/java/com/roshanadke/weekwatch/presentation/screens/PeopleListScreen.kt
@@ -1,17 +1,24 @@
 package com.roshanadke.weekwatch.presentation.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.roshanadke.weekwatch.common.BottomNavigationScreens
+import com.roshanadke.weekwatch.presentation.components.PersonItemCard
 import com.roshanadke.weekwatch.presentation.viewmodels.PeopleViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PeopleListScreen(
     navController: NavController,
@@ -21,15 +28,40 @@ fun PeopleListScreen(
 
     val peopleListUiState = viewModel.peopleListUiState
 
-    LazyColumn {
-        peopleListUiState.value.peopleListMainDto?.personDtoList?.let {
-            items(it) {
-                Text(text = it.name ?: "", Modifier.padding(12.dp))
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(text = "Popular People")
+            })
+        }
+    ) {
+        Box(Modifier.padding(it)) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.fillMaxSize(),
 
+                ) {
+                peopleListUiState.value.peopleListMainDto?.personDtoList?.let {
+                    items(it) {
+                        Text(text = it.name ?: "", Modifier.padding(12.dp))
 
+                        PersonItemCard(
+                            modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp),
+                            imageEndpoint = it.profilePath,
+                            personName = it.name,
+                            onCardClicked = {
+
+                            }
+                        )
+
+                    }
+                }
             }
+
         }
     }
+
+
 
 
 }
